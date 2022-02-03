fec1cf75f759e1430eb4f3d38e91bf611b2ec382
ioschristian <christian.d.tisby@gmail.com>
2022-02-03 09:22:16 -0500

    Start project 1 A Good Start

---
+++ AGoodStart/AGoodStart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AGoodStart
+//
+//  Created by Christian Tisby on 2/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
