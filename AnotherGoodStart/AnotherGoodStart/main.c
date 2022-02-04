4431a5485a6196b4423ea12a1ce681305bcd3a94
ioschristian <christian.d.tisby@gmail.com>
2022-02-04 16:17:28 -0500

    adding completed chapter 3 variables

---
+++ AnotherGoodStart/AnotherGoodStart/main.c
@@ -0,0 +1,20 @@
+//
+//  main.c
+//  AnotherGoodStart
+//
+//  Created by Christian Tisby on 2/3/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+   //Print the beginning of the novel
+    printf("It was the best of times.\n");
+    printf("It was the worst of times.\n");
+    /*
+     Is that actually any good?
+     Maybe it needs a rewrite.
+     */
+    
+    return 0;
+}
